7551b83edf800a7684cc1b23940d0a12676454a1
teranricardo <teranricardodavid@gmail.com>
2024-08-03 00:59:49 -0400

    Respuestas en el enrutador de cuentas de ahorro

--- controllers/savings.c.js
+++ controllers/savings.c.js
@@ -2,102 +2,102 @@ var savingsModel = require("../models/savings.m");
 var usersModel = require("../models/users.m");
 
 class SavingController {
-  createForm(req, res) {
-    usersModel.show()
-      .then((users) => {
-        res.render('savings/new', { users });
-      })
-      .catch((err) => res.status(500).send(`Error al obtener usuarios: ${err}`));
-  }
-
-  create(req, res) {
-    let saving = req.body;
-    if (!saving.userId || !saving.interestRate || !saving.balance) {
-      return res.status(405).send("Faltan datos de la cuenta de ahorro por agregar.");
+  async createForm() {
+    try {
+      const users = await usersModel.show();
+      return { users };
+    } catch (err) {
+      throw new Error(`Error al obtener usuarios: ${err}`);
     }
-    usersModel.showByID(saving.userId)
-      .then((user) => {
-        if (!user) {
-          return res.status(404).send(`No se encontró el usuario con ID: ${saving.userId}`);
-        }
-        saving.createdAt = new Date();
-        return savingsModel.create(saving)
-          .then(() => res.redirect('/savings'))
-          .catch((err) => res.status(500).send(`Error al crear la cuenta de ahorro: ${err}`));
-      })
-      .catch((err) => res.status(500).send(`Error al crear la cuenta de ahorro: ${err}`));
   }
 
-  show(req, res) {
-    savingsModel.show()
-      .then((savings) => res.render('savings/index', { savings }))
-      .catch((err) => res.status(500).send(`Error al listar cuentas de ahorro: ${err}`));
+  async create(data) {
+    const { userId, interestRate, balance } = data;
+    if (!userId || !interestRate || !balance) {
+      throw new Error("Faltan datos de la cuenta de ahorro por agregar.");
+    }
+    try {
+      const user = await usersModel.showByID(userId);
+      if (!user) {
+        throw new Error(`No se encontró el usuario con ID: ${userId}`);
+      }
+      const saving = {
+        userId,
+        interestRate,
+        balance,
+        createdAt: new Date()
+      };
+      await savingsModel.create(saving);
+      return { success: true };
+    } catch (err) {
+      throw new Error(`Error al crear la cuenta de ahorro: ${err}`);
+    }
   }
 
-  showByID(req, res) {
-    const id = req.params.id;
-
-    savingsModel.showByID(id)
-      .then((saving) => {
-        if (!saving) {
-          return res.status(404).send(`No se encontró la cuenta de ahorro con id: ${id}`);
-        }
-        res.render('savings/show', { saving });
-      })
-      .catch((err) => res.status(500).send(`Error al buscar la cuenta de ahorro: ${err}`));
+  async show() {
+    try {
+      const savings = await savingsModel.show();
+      return { savings };
+    } catch (err) {
+      throw new Error(`Error al listar cuentas de ahorro: ${err}`);
+    }
   }
 
-  edit(req, res) {
-    const id = req.params.id;
-
-    Promise.all([savingsModel.showByID(id), usersModel.show()])
-      .then(([saving, users]) => {
-        if (!saving) {
-          return res.status(404).send(`No se encontró la cuenta de ahorro con id: ${id}`);
-        }
-        res.render('savings/edit', { saving, users });
-      })
-      .catch((err) => res.status(500).send(`Error al cargar la cuenta de ahorro para editar: ${err}`));
+  async showByID(id) {
+    try {
+      const saving = await savingsModel.showByID(id);
+      if (!saving) {
+        throw new Error(`No se encontró la cuenta de ahorro con id: ${id}`);
+      }
+      return { saving };
+    } catch (err) {
+      throw new Error(`Error al buscar la cuenta de ahorro: ${err}`);
+    }
   }
 
-  update(req, res) {
-    const id = req.params.id;
-    const updatedSaving = req.body;
-
-    savingsModel.showByID(id)
-      .then((saving) => {
-        if (!saving) {
-          return res.status(404).send(`No se encontró la cuenta de ahorro con id: ${id}`);
-        }
-
-        const newSaving = {
-          id: id,
-          userId: updatedSaving.userId ? updatedSaving.userId : saving.userId,
-          interestRate: updatedSaving.interestRate ? updatedSaving.interestRate : saving.interestRate,
-          createdAt: saving.createdAt,
-          balance: updatedSaving.balance ? updatedSaving.balance : saving.balance,
-        };
-
-        return savingsModel.edit(newSaving, id)
-          .then(() => res.redirect(`/savings/${id}`))
-          .catch((err) => res.status(500).send(`Error al editar la cuenta de ahorro: ${err}`));
-      })
-      .catch((err) => res.status(500).send(`Error al editar la cuenta de ahorro: ${err}`));
+  async edit(id) {
+    try {
+      const [saving, users] = await Promise.all([savingsModel.showByID(id), usersModel.show()]);
+      if (!saving) {
+        throw new Error(`No se encontró la cuenta de ahorro con id: ${id}`);
+      }
+      return { saving, users };
+    } catch (err) {
+      throw new Error(`Error al cargar la cuenta de ahorro para editar: ${err}`);
+    }
   }
 
-  delete(req, res) {
-    const id = req.params.id;
+  async update(id, data) {
+    try {
+      const saving = await savingsModel.showByID(id);
+      if (!saving) {
+        throw new Error(`No se encontró la cuenta de ahorro con id: ${id}`);
+      }
+      const updatedSaving = {
+        id,
+        userId: data.userId || saving.userId,
+        interestRate: data.interestRate || saving.interestRate,
+        createdAt: saving.createdAt,
+        balance: data.balance || saving.balance,
+      };
+      await savingsModel.edit(updatedSaving, id);
+      return { success: true };
+    } catch (err) {
+      throw new Error(`Error al editar la cuenta de ahorro: ${err}`);
+    }
+  }
 
-    savingsModel.showByID(id)
-      .then((saving) => {
-        if (!saving) {
-          return res.status(404).send(`No se encontró la cuenta de ahorro con id: ${id}`);
-        }
-        return savingsModel.delete(id)
-          .then(() => res.redirect('/savings'))
-          .catch((err) => res.status(500).send(`Error al eliminar la cuenta de ahorro: ${err}`));
-      })
-      .catch((err) => res.status(500).send(`Error al eliminar la cuenta de ahorro: ${err}`));
+  async delete(id) {
+    try {
+      const saving = await savingsModel.showByID(id);
+      if (!saving) {
+        throw new Error(`No se encontró la cuenta de ahorro con id: ${id}`);
+      }
+      await savingsModel.delete(id);
+      return { success: true };
+    } catch (err) {
+      throw new Error(`Error al eliminar la cuenta de ahorro: ${err}`);
+    }
   }
 }
 
--- routes/savings.js
+++ routes/savings.js
@@ -4,30 +4,78 @@ var savingsController = require("../controllers/savings.c");
 const { verifyToken, verifyRole } = require('../middlewares/auth');
 
 /* GET lista de cuentas de ahorro */
-router.get('/', verifyToken, savingsController.show);
+router.get('/', verifyToken, async (req, res) => {
+  try {
+    const { savings } = await savingsController.show();
+    res.render('savings/index', { savings });
+  } catch (err) {
+    res.status(500).send(`Error al listar cuentas de ahorro: ${err}`);
+  }
+});
 
 /* GET formulario de creación de cuenta de ahorro */
-router.get('/new', verifyToken, verifyRole('admin'), savingsController.createForm);
+router.get('/new', verifyToken, verifyRole('admin'), async (req, res) => {
+  try {
+    const { users } = await savingsController.createForm();
+    res.render('savings/new', { users });
+  } catch (err) {
+    res.status(500).send(`Error al obtener usuarios: ${err}`);
+  }
+});
 
 /* POST crear nueva cuenta de ahorro */
-router.post('/', verifyToken, verifyRole('admin'), savingsController.create);
+router.post('/', verifyToken, verifyRole('admin'), async (req, res) => {
+  try {
+    await savingsController.create(req.body);
+    res.redirect('/savings');
+  } catch (err) {
+    res.status(500).send(`Error al crear la cuenta de ahorro: ${err}`);
+  }
+});
 
 /* GET detalle de cuenta de ahorro por ID */
-router.get('/:id', verifyToken, savingsController.showByID);
+router.get('/:id', verifyToken, async (req, res) => {
+  try {
+    const { saving } = await savingsController.showByID(req.params.id);
+    res.render('savings/show', { saving });
+  } catch (err) {
+    res.status(500).send(`Error al buscar la cuenta de ahorro: ${err}`);
+  }
+});
 
 /* GET formulario de edición de cuenta de ahorro */
-router.get('/:id/edit', verifyToken, verifyRole('admin'), savingsController.edit);
+router.get('/:id/edit', verifyToken, verifyRole('admin'), async (req, res) => {
+  try {
+    const { saving, users } = await savingsController.edit(req.params.id);
+    res.render('savings/edit', { saving, users });
+  } catch (err) {
+    res.status(500).send(`Error al cargar la cuenta de ahorro para editar: ${err}`);
+  }
+});
 
 /* PUT actualizar cuenta de ahorro */
-router.put('/:id', verifyToken, verifyRole('admin'), savingsController.update);
+router.put('/:id', verifyToken, verifyRole('admin'), async (req, res) => {
+  try {
+    await savingsController.update(req.params.id, req.body);
+    res.redirect(`/savings/${req.params.id}`);
+  } catch (err) {
+    res.status(500).send(`Error al editar la cuenta de ahorro: ${err}`);
+  }
+});
 
 /* GET confirmación de eliminación */
 router.get('/:id/delete', verifyToken, verifyRole('admin'), (req, res) => {
-  const savingId = req.params.id;
-  res.render('savings/delete', { savingId });
+  res.render('savings/delete', { savingId: req.params.id });
 });
 
 /* DELETE eliminar cuenta de ahorro */
-router.delete('/:id', verifyToken, verifyRole('admin'), savingsController.delete);
+router.delete('/:id', verifyToken, verifyRole('admin'), async (req, res) => {
+  try {
+    await savingsController.delete(req.params.id);
+    res.redirect('/savings');
+  } catch (err) {
+    res.status(500).send(`Error al eliminar la cuenta de ahorro: ${err}`);
+  }
+});
 
 module.exports = router;
